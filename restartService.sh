e22499aea3cf8d2885b9b7a17254f9a53e62d20a
nightwish2016 <kzhou2017@outlook.com>
2025-07-27 12:24:33 +0800

    update restartservice script

--- restartService.sh
+++ restartService.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
+pyenv shell myenv3.12  # 或用 source activate
+
+cd /root/myai || exit
 git pull
-pkill gunicorn
-gunicorn -w 4 -b 0.0.0.0:5000 --timeout 120 --error-logfile /root/myai/gunicorn/err.log --log-file /root/myai/gunicorn/gunicorn.log "app:create_app()" --log-level debug &
+pkill -f gunicorn
+sleep 2
+
+nohup gunicorn -w 4 -b 0.0.0.0:5000 \
+  --timeout 120 \
+  --error-logfile /root/myai/gunicorn/err.log \
+  --access-logfile /root/myai/gunicorn/gunicorn.log \
+  "app:create_app" \
+  --log-level debug > /dev/null 2>&1 &
