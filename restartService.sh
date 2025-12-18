ee8ff562a57a6db1d6caf7591f556160e4c26fdc
nightwish2016 <kzhou2017@outlook.com>
2025-12-18 08:43:14 +0000

    update restartService.sh

--- restartService.sh
+++ restartService.sh
@@ -4,14 +4,14 @@ export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 
-pyenv shell myenv3.12  # 或用 source activate
+#pyenv shell myenv3.12  # 或用 source activate
 
 
 pkill -f gunicorn
 sleep 2
 
 # 使用nohup让进程在后台持续运行，即使SSH断开也不会停止
-nohup gunicorn -w 4 -b 0.0.0.0:5000 --timeout 120 --error-logfile /root/myai/gunicorn/err.log --log-file /root/myai/gunicorn/gunicorn.log "app:create_app()" --log-level debug > /dev/null 2>&1 &
+nohup gunicorn -w 4 -b 0.0.0.0:5000 --timeout 120 --error-logfile /root/myweb/AIChatRobot/gunicorn/err.log --log-file /root/myweb/AIChatRobot/gunicorn/gunicorn.log "app:create_app()" --log-level debug > /dev/null 2>&1 &
 
 echo "服务已启动，进程ID: $!"
-echo "即使关闭SSH连接，服务也会继续运行"
+echo "即使关闭SSH连接，服务也会继续运行" 
